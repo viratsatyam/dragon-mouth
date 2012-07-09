0db83e268d54ea2e5652cb2ac36aa8cc2cc6b283
ravisharma1987 <ravisharma1987@gmail.com>
2012-07-09 09:42:38 +0530

    Non check out commit, just for code accessibility.

---
+++ Fuel Manager/src/fuel/manager/app/home/HomeActivity.java
@@ -0,0 +1,18 @@
+package fuel.manager.app.home;
+
+import fuel.manager.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_home);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
